fb349f431f3028cd4d2adf3af5cfa25f6756e79d
sarang8806 <patrangesarang@gmail.com>
2019-01-23 17:39:55 +0530

    server and client running successfully, need to optimize

--- src/vehicle_data_client.cpp
+++ src/vehicle_data_client.cpp
@@ -1,33 +1,41 @@
 #include "ros/ros.h"
 #include "user_story_one/vehicle_data.h"
 #include <cstdlib>
+#include <iostream>
+#include <cstring>
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "vehicle_data_client");
-  if (argc != 3)
+  if (argc != 2)
   {
     ROS_INFO("usage: data required");
     return 1;
   }
 
   ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<user_story_one::vehicle_data>("add_two_ints");
+  ros::ServiceClient client = n.serviceClient<user_story_one::vehicle_data>("vehicle_data");
   user_story_one::vehicle_data srv;
-  srv.request.timestamp= atoll(argv[1]);
+  srv.request.timestamp = (std::string)argv[1];
   if (client.call(srv))
   {
-    ROS_INFO("Vehicle speed: %s", (string)srv.response.vehicle_speed);
-    ROS_INFO("Engine speed: %s", (string)srv.response.engine_speed);
-    ROS_INFO("Driver door status: %s", (string)srv.response.driver_door_status);
-    ROS_INFO("Front passenger door status: %s", (string)srv.response.front_passenger_door_status);
-    ROS_INFO("Rear left door status: %s", (string)srv.response.rear_left_door_status);
-    ROS_INFO("Rear right door status: %s", (string)srv.response.rear_right_door_status);
+    /*ROS_INFO("Vehicle speed: %s", (std::string)srv.response.vehicle_speed);
+    ROS_INFO("Engine speed: %s", (std::string)srv.response.engine_speed);
+    ROS_INFO("Driver door status: %s", (std::string)srv.response.driver_door_status);
+    ROS_INFO("Front passenger door status: %s", srv.response.front_passenger_door_status);
+    ROS_INFO("Rear left door status: %s", srv.response.rear_left_door_status);
+    ROS_INFO("Rear right door status: %s", srv.response.rear_right_door_status); */
 
+    std::cout<<"Vehicle speed:               "<< srv.response.vehicle_speed<<std::endl;
+    std::cout<<"Engine speed:                "<< (std::string)srv.response.engine_speed<<std::endl;
+    std::cout<<"Driver door status:          "<< (std::string)srv.response.driver_door_status<<std::endl;
+    std::cout<<"Front passenger door status: "<< srv.response.front_passenger_door_status<<std::endl;
+    std::cout<<"Rear left door status:       "<< srv.response.rear_left_door_status<<std::endl;
+    std::cout<<"Rear right door status:      "<< srv.response.rear_right_door_status<<std::endl;
   }
   else
   {
-    ROS_ERROR("Failed to call service add_two_ints");
+    ROS_ERROR("Failed to call service vehicle_data");
     return 1;
   }
 
--- src/vehicle_data_server.cpp
+++ src/vehicle_data_server.cpp
@@ -1,47 +1,63 @@
 #include "ros/ros.h"
 #include <fstream>
 #include <string>
+#include <iostream>
 #include "user_story_one/vehicle_data.h"
 
 bool vehicle_status(user_story_one::vehicle_data::Request  &req,
         user_story_one::vehicle_data::Response  &res)
 {
-   ifstream infile;
-   string timestamp;
+   std::ifstream infile;
+   std::string timestamp;
+   bool ret_val = true;
    
-   infile.open("can_data.csv");
-   if(infile.is_open())
-     ROS_INFO("File is open!"); 
-   else
-     ROS_INFO("File open error"); 
-
-   while(!infile.eof())
+   ROS_INFO("Ready to receive inputs from client.");
+   infile.open("/home/sarangp1/catkin_ws/src/user_story_one/src/can_data.csv",std::ios::in);
+   if(infile)
    {
-      getline(infile,res.msg_id,',');
-      getline(infile,res.vehicle_speed,',');
-      getline(infile,res.engine_speed,',');
-      getline(infile,res.driver_door_status,',');
-      getline(infile,res.front_passenger_door_status,',');
-      getline(infile,res.rear_left_door_status,',');
-      getline(infile,res.rear_right_door_status,',');
-      infile>>timestamp;
-      
-      if(timestamp == req.timestamp)
-      {  
-        ROS_INFO("Request: timestamp=%ld", (long int)req.timestamp);
-        ROS_INFO("Sending back response:\n");
-        ROS_INFO("msg_id:                      %s", res.msg_id);
-        ROS_INFO("vehicle_speed:               %s", res.vehicle_speed);
-        ROS_INFO("engine_speed:                %s", res.engine_speed);
-        ROS_INFO("driver_door_status:          %s", res.driver_door_status);
-        ROS_INFO("front_passenger_door_status: %s", res.front_passenger_door_status);
-        ROS_INFO("rear_left_door_status:       %s", res.rear_left_door_status);
-        ROS_INFO("rear_right_door_status:      %s", res.rear_right_door_status);
-        break;
+      while(!infile.eof())
+      {
+        getline(infile,res.msg_id,',');
+        getline(infile,res.vehicle_speed,',');
+        getline(infile,res.engine_speed,',');
+        getline(infile,res.driver_door_status,',');
+        getline(infile,res.front_passenger_door_status,',');
+        getline(infile,res.rear_left_door_status,',');
+        getline(infile,res.rear_right_door_status,',');
+        infile>>timestamp;
+
+        if(timestamp == req.timestamp)
+        {  
+          //ROS_INFO("Request: timestamp=%s",(std::string)req.timestamp);
+          /*std::cout<<"Request: timestamp="<<req.timestamp<<std::endl;
+          ROS_INFO("Sending back response:\n");
+          ROS_INFO("msg_id:                      %s", (std::string)res.msg_id);
+          ROS_INFO("vehicle_speed:               %s", (std::string)res.vehicle_speed);
+          ROS_INFO("engine_speed:                %s", res.engine_speed);
+          ROS_INFO("driver_door_status:          %s", res.driver_door_status);
+          ROS_INFO("front_passenger_door_status: %s", res.front_passenger_door_status);
+          ROS_INFO("rear_left_door_status:       %s", res.rear_left_door_status);
+          ROS_INFO("rear_right_door_status:      %s", res.rear_right_door_status); */
+
+	  std::cout<<"Request: timestamp="<<req.timestamp<<std::endl;
+          std::cout<<"Sending back response:\n";
+          std::cout<<"msg_id:                      " <<res.msg_id<<std::endl;
+          std::cout<<"vehicle_speed:               " <<res.vehicle_speed<<std::endl;
+          std::cout<<"engine_speed:                " <<res.engine_speed<<std::endl;
+          std::cout<<"driver_door_status:          " <<res.driver_door_status<<std::endl;
+          std::cout<<"front_passenger_door_status: " <<res.front_passenger_door_status<<std::endl;
+          std::cout<<"rear_left_door_status:       " <<res.rear_left_door_status<<std::endl;
+          std::cout<<"rear_right_door_status:      " <<res.rear_right_door_status<<std::endl;
+          break;
+        }
       }
    }
-
-   return true;
+   else
+   {
+     ROS_INFO("File open error");
+     ret_val = false;
+   }
+   return ret_val;
 }
 
 int main(int argc, char **argv)
